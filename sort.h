f88a18915b845f8873f884984103fd02d03b1583
ayandasibanda <ayanda.sibanda17@gmail.com>
2023-09-30 13:26:49 -0700

    Adding files

--- sort.h
+++ sort.h
@@ -1,11 +1,58 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
-/* Function prototypes */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element in the list
+ * @next: Pointer to the next element in the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm.
+ * @array: The array to be sorted
+ * @size: Size of the array
+ */
 void bubble_sort(int *array, size_t size);
+
+/**
+ * print_array - Prints an array of integers
+ * @array: The array to be printed
+ * @size: Size of the array
+ */
 void print_array(const int *array, size_t size);
+
+/**
+ * print_list - Prints a doubly linked list of integers
+ * @list: Pointer to the head of the list
+ */
+void print_list(const listint_t *list);
+
+/**
+ * swap_node - Swaps a doubly linked list node with its previous node
+ * @node: The node to be swapped
+ * @list: Pointer to the head of the list
+ * @return: Pointer to the new head of the list
+ */
+listint_t *swap_node(listint_t *node, listint_t **list);
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the Insertion sort algorithm.
+ * @list: Pointer to a pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list);
+
 
 #endif /* SORT_H */
 
