6c9204c750d772cfcec77acda04a3ed656237d69
ayandasibanda <ayanda.sibanda17@gmail.com>
2023-09-30 19:21:41 -0700

    Adding files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,51 +1,17 @@
-/* sort.h */
-
-#ifndef SORT_H
-#define SORT_H
-
-#include <stddef.h>
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element in the list
- * @next: Pointer to the next element in the list
- */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
-/* Sorting algorithms */
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-
-#endif /* SORT_H */
-
-/* 2-selection_sort.c */
-
 #include "sort.h"
 
 /**
- * selection_sort - function that sorts an array of integers in ascending
- * order using the Selection sort algorithm
- * @size: size of the array
- * @array: list with numbers
+ * selection_sort - Sorts an array of integers in ascending order using
+ * the Selection Sort algorithm.
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
  */
 void selection_sort(int *array, size_t size)
 {
     size_t i, index;
     int tmp, swap, flag = 0;
 
-    if (array == NULL)
+    if (array == NULL || size < 2)
         return;
 
     for (i = 0; i < size; i++)
@@ -53,6 +19,7 @@ void selection_sort(int *array, size_t size)
         tmp = i;
         flag = 0;
 
+        /* Find the index of the minimum element in the unsorted part */
         for (index = i + 1; index < size; index++)
         {
             if (array[tmp] > array[index])
@@ -62,59 +29,14 @@ void selection_sort(int *array, size_t size)
             }
         }
 
+        /* Swap the found minimum element with the first element */
         swap = array[i];
         array[i] = array[tmp];
         array[tmp] = swap;
 
+        /* Print the array after each swap if any swaps occurred */
         if (flag != 0)
             print_array(array, size);
     }
-}
-
-/* print_array.c */
-
-#include "sort.h"
-#include <stdio.h>
-
-/**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: Size of the array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
-}
-
-/* 2-main.c */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "sort.h"
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    selection_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
 }
 
